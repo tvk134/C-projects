d6c0f01f39f01b3acb695ffb0f8e33c17e09b3ee
tvk134 <46226474+tvk134@users.noreply.github.com>
2023-01-09 13:55:12 +0800

    Add files via upload
    C program to check whether numbers supplied in a txt file are truncating prime numbers or not.

---
+++ truncatingPrime/truncatingPrime.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int checker(int a){
+    if(a<=1)
+        return 0;
+    
+    while(a!=0)
+    {
+        for(int i = 2;i<=(a/2);i++){
+            if(a%i==0)
+                return 0;
+        }
+        a=a/10;
+    }
+    return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+    if(argc<2){
+        printf("ERROR: INSUFFICIENT ARGUMENTS\n");
+        return EXIT_SUCCESS;
+    }
+
+    FILE* fp = fopen(argv[1],"r");
+    
+    if(fp==NULL){
+        printf("error\n");
+        return EXIT_FAILURE;
+    }
+    
+    int lines;
+    fscanf(fp,"%d\n",&lines);
+
+    while(lines!=0){
+        
+        int num;
+        fscanf(fp,"%d\n",&num);
+
+        if(checker(num) == 0)
+            printf("no\n");
+        else
+            printf("yes\n");
+
+        lines--;
+    }
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
